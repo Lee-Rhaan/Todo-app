bfa198836be568d55c4cb1e4f979986c7473062f
Lee-Rhaan <rhaanking@gmail.com>
2025-08-06 19:05:11 +0200

    Updated H2 db

--- src/test/java/com/example/todoapp/api/integration/TodoIntegrationTest.java
+++ src/test/java/com/example/todoapp/api/integration/TodoIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * a full Spring Boot context.
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("junit")
 class TodoIntegrationTest {
 
     /**
